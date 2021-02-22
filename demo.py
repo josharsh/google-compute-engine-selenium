b9bf925a7c586a5d432669c6bf36e33bb850fee2
Harsh Joshi <harsh.joshi.pth@gmail.com>
2021-02-22 15:49:47 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,29 +1,37 @@
-#!/usr/bin/env python
-
-from pyvirtualdisplay import Display
 from selenium import webdriver
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+# open it, go to a website, and get results
+browser = webdriver.Chrome('chromedriver',options=options)
+states = ['ANDAMAN & NICOBAR ISLANDS']
+    
 
-display = Display(visible=0, size=(800, 600))
-display.start()
+browser.get('https://www.icicilombard.com/IL-HEALTH-CARE/Customer/GetHospitalList')
 
-url = 'http://www.python.org'
+hospitalData = []
 
-print 'browsing with firefox, ', url
-try:
-  browser = webdriver.Firefox()
-  browser.get(url)
-  print browser.title
-  browser.quit()
-except Exception as e:
-  print e
+hos = dict()
 
-print 'browsing with chrome, ', url
-try:
-  browser = webdriver.Chrome()
-  browser.get(url)
-  print browser.title
-  browser.quit()
-except Exception as e:
-  print e
-
-display.stop()
+for state in states:
+    
+    select = Select(browser.find_element_by_id('ddlStateList'))
+    select.select_by_visible_text(state)
+    table = browser.find_element_by_id('ddlStateList')
+    div = browser.find_element_by_id('divSearchData')
+    hospital_names_list=browser.find_elements_by_class_name('clsHospName')
+    address_names_list=browser.find_elements_by_class_name('clsAddress')
+    city_names_list=browser.find_elements_by_class_name('clsCity')
+    area_names_list=browser.find_elements_by_class_name('clsArea')
+    state_names_list=browser.find_elements_by_class_name('clsState')
+    for i in range(len(hospital_names_list)):
+      tempData = {
+        "name": hospital_names_list[i].text,
+        "city": city_names_list[i].text,
+        "area":  area_names_list[i].text,
+        "state":  state_names_list[i].text
+      }
+      hospitalData.append(tempData)
+    print(hospitalData)
+    print(len(hospitalData))
